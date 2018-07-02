d84da58b05de84429993a7fb740baddab95f0522
Josue V. Alorro <joalorro@gmail.com>
2018-07-02 17:07:56 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,6 +42,6 @@ def using_delete (array, string)
   array.delete(string)
 end
 
-def using_delete_at(array,index)
-  array.delete[index - 1]
+def using_delete_at(array,int)
+  array[int].delete
 end
