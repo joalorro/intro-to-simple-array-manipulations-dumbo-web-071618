ebe2459f06e8c79f682e7a98b678afa8a78cd9e8
Josue V. Alorro <joalorro@gmail.com>
2018-07-02 17:02:27 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -18,7 +18,7 @@ def using_shift (array)
   array.shift 
 end
 
-def using_shift_with_args (array)
+def shift_with_args (array)
   array.shift(2)
 end
 
@@ -27,7 +27,7 @@ def using_concat(arr1, arr2)
 end
 
 def using_insert(array, element)
-  array.insert(3,element)
+  array.insert(4,element)
 end
 
 def using_uniq(array)
@@ -38,6 +38,6 @@ def using_flatten(array)
   array.flatten 
 end
 
-def using_delete(array,string)
+def using_delete_at(array,string)
   array.delete(string)
 end
